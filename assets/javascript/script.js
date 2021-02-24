ea1eece8c5f9a129d7435505f1d1a31568c27270
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-02-24 12:05:11 -0600

    Add quiz complete sect, emplyed some DRY

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,3 @@
-var startQuizEl = document.querySelector(".start-button");
-startQuizEl.addEventListener("click", activateQuiz);
 var selectedAnswer = document.querySelector("#answer-catcher");
 selectedAnswer.addEventListener("click", quizHandler);
 var questions = ["1+1", "3+2", "8*2"];
@@ -17,48 +15,74 @@ var scoreKeeper = setInterval(() => {
     }
 }, 1000);
 
-function activateQuiz(event){
-    event.stopPropagation();
-    event.preventDefault();
-    active = true;
-    var formEl = document.querySelector(".option-form");
-    var infoPEl = document.querySelector(".info");
-    var quizContentContainerEl = document.querySelector(".quiz-container");
-    quizContentContainerEl.className = "quiz-container";
-    startQuizEl.remove();
-    infoPEl.remove();
-    questionBuilder();
-    answerBuilder(formEl);
-    currentQuestion++;
-}
-
 function quizHandler(event){
-    event.preventDefault();
-    var formEl = document.querySelector(".option-form");
-    var answerResult = document.createElement("div");
-    answerResult.className = "answer-result";
-
-    if(event.target.getAttribute("data-correct-answer") === "y"){
-        answerResult.textContent = "Correct!";
-    }
-    else{
-        answerResult.textContent = "Incorrect!";
-    }
-
-    formEl.appendChild(answerResult);
-    setTimeout(() => {
-        answerResult.remove();
-        answerDestroyer();
+    if(event.target.matches(".start-button")){
+        event.stopPropagation();
+        active = true;
+        var formEl = document.querySelector(".option-form");
+        var infoPEl = document.querySelector(".info");
+        var quizContentContainerEl = document.querySelector(".quiz-container");
+        quizContentContainerEl.className = "quiz-container";
+        event.target.remove();
+        infoPEl.remove();
         questionBuilder();
         answerBuilder(formEl);
-    }, 1500);
-
-    if(currentQuestion !== questions.length-1) currentQuestion++;
-    else{
-        answerDestroyer();
-        clearInterval(scoreKeeper);
-        
+        currentQuestion++;
     }
+    if(event.target.matches(".option")){
+        var formEl = document.querySelector(".option-form");
+        var answerResult = document.createElement("div");
+        answerResult.className = "answer-result";
+
+        if(event.target.getAttribute("data-correct-answer") === "y"){
+            answerResult.textContent = "Correct!";
+        }
+        else{
+            answerResult.textContent = "Incorrect!";
+        }
+
+        formEl.appendChild(answerResult);
+        selectedAnswer.removeEventListener("click", quizHandler);
+        setTimeout(() => {
+            answerResult.remove();
+            if(currentQuestion <= questions.length-1){
+                answerDestroyer();
+                questionBuilder();
+                answerBuilder(formEl);
+                currentQuestion++;
+                selectedAnswer.addEventListener("click", quizHandler);
+            }
+            else{
+                currentQuestion = 0;
+                answerDestroyer();
+                clearInterval(scoreKeeper);
+                writeInitials(formEl);
+            }
+        }, 1500);
+    }
+}
+
+function writeInitials(formEl){
+    var questionH2El = document.querySelector("#question");
+    questionH2El.textContent = "Quiz Completed!";
+
+    var gameComleteContainerEl = document.createElement("div");
+    gameComleteContainerEl.id = "input-catcher";
+
+    var initialsInputEl = document.createElement("input");
+    initialsInputEl.name = "initials";
+    initialsInputEl.placeholder = "Your Initials Here...";
+    initialsInputEl.type = "text";
+    gameComleteContainerEl.appendChild(initialsInputEl);
+
+    var gameComleteInfoEl = document.createElement("p");
+    gameComleteInfoEl.className = "info";
+    gameComleteInfoEl.style.color = "var(--questionTextColor)";
+    gameComleteInfoEl.textContent = "You've Finished the quiz! Please enter your initials below to save your score: ";
+
+    formEl.appendChild(gameComleteInfoEl);
+    formEl.appendChild(gameComleteContainerEl);
+
 }
 
 function questionBuilder(){
@@ -77,6 +101,7 @@ function answerBuilder(formEl){
         else{
             answerButtonEl.setAttribute("data-correct-answer", "n");
         }
+        answerButtonEl.type = "button";
         answerButtonEl.setAttribute("data-id", i);
         answerButtonEl.textContent = answers[currentQuestion][i];
         formEl.appendChild(answerButtonEl);
